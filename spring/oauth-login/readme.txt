71d2283e2cd425f8964fb5a31f297d7487227434
胡睿毅 <807776962@qq.com>
2023-11-21 09:31:54 +0800

    oauth-login

---
+++ spring/oauth-login/readme.txt
@@ -0,0 +1,10 @@
+https://github.com/xkcoding/third-login
+
+github_login
+https://github.com/lenve/oauth2-samples
+
+withjpa
+https://github.com/lenve/spring-security-samples
+
+gitee_login 
+https://gitee.com/zhengqingya/java-workspace.git
---
+++ spring/oauth-login/withjpa/src/main/java/org/javaboy/HelloController.java
@@ -0,0 +1,42 @@
+package org.javaboy;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+/**
+ * @作者 江南一点雨
+ * @公众号 江南一点雨
+ * @微信号 a_java_boy
+ * @GitHub https://github.com/lenve
+ * @博客 http://wangsong.blog.csdn.net
+ * @网站 http://www.javaboy.org
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/admin/hello")
+    public String admin() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+        String username = authentication.getName();
+        Object principal = authentication.getPrincipal();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+        return "admin";
+    }
+
+    @GetMapping("/user/hello")
+    public String user() {
+        return "user";
+    }
+}
