668cc3a7234e38552c818a564fcb21968796eb2f
胡睿毅 <807776962@qq.com>
2024-04-21 23:34:44 +0800

    pro-spring-5-ch16

---
+++ pro-spring-5/chapter16/singer-webapp-jcfg/src/main/java/com/apress/prospring5/ch16/init/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.apress.prospring5.ch16.init;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
