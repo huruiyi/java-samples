436af7e16e8faf10048a57859558de0f76f74d4a
胡睿毅 <807776962@qq.com>
2024-04-25 10:16:16 +0800

    rabbit

--- spring-boot-v3/spring-cloud-stream-rabbit-consumer/src/main/java/vip/fairy/consumers/NameSinkConfiguration.java
+++ spring-boot-v3/spring-cloud-stream-rabbit-consumer/src/main/java/vip/fairy/consumers/NameSinkConfiguration.java
@@ -11,28 +11,54 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
 import vip.fairy.model.Person;
 
+/**
+ * {"name":"tn01","processedTimestamp":12345}
+ */
 @Configuration
 public class NameSinkConfiguration {
 
   Logger log = LoggerFactory.getLogger(NameSinkConfiguration.class);
 
-  /**
-   * {"name":"tn01","processedTimestamp":12345}
-   */
   @Bean
-  Consumer<Message<Person>> ackMessage() {
-    log.info("ackMessage-初始化订阅");
+  public Consumer<Person> phoneMessage() {
+    return person -> {
+            log.info("phoneMessage-消息消费成功：  Payload:{}", person);
+      System.out.println(person.getName() + "---" + person.getProcessedTimestamp());
+    };
+  }
+
+  @Bean
+  public Consumer<Message<Person>> mailMessage() {
+    return person -> {
+      Channel channel = person.getHeaders().get(AmqpHeaders.CHANNEL, Channel.class);
+      Long deliveryTag = person.getHeaders().get(AmqpHeaders.DELIVERY_TAG, Long.class);
+      log.info("mailMessage-消息消费成功：  Payload:{}", person.getPayload());
+      try {
+        channel.basicAck(deliveryTag, false);
+      } catch (IOException e) {
+        try {
+          channel.basicAck(deliveryTag, true);
+        } catch (IOException ex) {
+          throw new RuntimeException(ex);
+        }
+      }
+    };
+  }
+
+  @Bean
+  public Consumer<Message<Person>> smsMessage() {
+    log.info("smsMessage-初始化订阅");
     return obj -> {
       Channel channel = obj.getHeaders().get(AmqpHeaders.CHANNEL, Channel.class);
       Long deliveryTag = obj.getHeaders().get(AmqpHeaders.DELIVERY_TAG, Long.class);
       try {
         long currentTimeMillis = System.currentTimeMillis();
         if (channel != null && currentTimeMillis % 2 == 0) {
-          log.info("ackMessage-消息消费成功：currentTimeMillis: {}, Payload:{}", currentTimeMillis, obj.getPayload());
+          log.info("smsMessage-消息消费成功：currentTimeMillis: {}, Payload:{}", currentTimeMillis, obj.getPayload());
           channel.basicAck(deliveryTag, false);
         } else {
           //丢弃
-          log.info("ackMessage-消息消费失败：currentTimeMillis: {}, Payload:{}", currentTimeMillis, obj.getPayload());
+          log.info("smsMessage-消息消费失败：currentTimeMillis: {}, Payload:{}", currentTimeMillis, obj.getPayload());
           channel.basicReject(deliveryTag, true);
         }
       } catch (Exception e) {
