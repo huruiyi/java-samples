f84c9c03efce194d75bbc63ce10082b27a4734a7
胡睿毅 <807776962@qq.com>
2024-05-23 17:19:19 +0800

    packt clean

--- spring-boot-v2/pet-store-v2/src/main/java/com/example/petstore/PetStoreApplicationV2.java
+++ spring-boot-v2/pet-store-v2/src/main/java/com/example/petstore/PetStoreApplicationV2.java
@@ -1,13 +1,30 @@
 package com.example.petstore;
 
+import com.example.petstore.model.Pet;
+import com.example.petstore.repository.PetRepository;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PetStoreApplicationV2 {
+public class PetStoreApplicationV2 implements CommandLineRunner {
 
   public static void main(String[] args) {
     SpringApplication.run(PetStoreApplicationV2.class, args);
   }
 
+  @Autowired
+  private PetRepository petRepository;
+
+  @Override
+  public void run(String... args) {
+    Pet pet1 = Pet.builder().id(UUID.randomUUID().toString()).cost(10L).numberInStock(100).itemName("小花猫").build();
+    petRepository.save(pet1);
+    System.out.println(pet1.getId());
+    Pet pet2 = Pet.builder().id(UUID.randomUUID().toString()).cost(20L).numberInStock(100).itemName("小狗子").build();
+    petRepository.save(pet2);
+    System.out.println(pet2.getId());
+  }
 }
--- spring-boot-v2/pet-store-v2/src/main/java/com/example/petstore/model/Pet.java
+++ spring-boot-v2/pet-store-v2/src/main/java/com/example/petstore/model/Pet.java
@@ -1,12 +1,16 @@
 package com.example.petstore.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Builder
 @RequiredArgsConstructor
+@AllArgsConstructor
 @Document(collection = "pet")
 public class Pet {
     private @Id String id;
