63bbb4065005626c7fcdc87f123b25e8c0f3a2cd
胡睿毅 <807776962@qq.com>
2024-05-27 22:21:22 +0800

    Spring Validator

--- pro-spring-5-2/dir-jsr/spring-validator/src/main/java/com/apress/prospring5/ch10/SpringValidatorDemo.java
+++ pro-spring-5-2/dir-jsr/spring-validator/src/main/java/com/apress/prospring5/ch10/SpringValidatorDemo.java
@@ -1,6 +1,7 @@
 package com.apress.prospring5.ch10;
 
 import com.apress.prospring5.ch10.config.AppConfig;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -10,28 +11,26 @@ import org.springframework.validation.ObjectError;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
-import java.util.List;
-
 public class SpringValidatorDemo {
 
-	private static Logger logger = LoggerFactory.getLogger(SpringValidatorDemo.class);
+  private static final Logger logger = LoggerFactory.getLogger(SpringValidatorDemo.class);
 
-	public static void main(String... args) {
-		GenericApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+  public static void main(String... args) {
+    GenericApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
 
-		Singer singer = new Singer();
-		singer.setFirstName(null);
-		singer.setLastName("Mayer");
+    Singer singer = new Singer();
+    singer.setFirstName(null);
+    singer.setLastName("Mayer");
 
-		Validator singerValidator = ctx.getBean("singerValidator", Validator.class);
-		BeanPropertyBindingResult result = new BeanPropertyBindingResult(singer, "John");
+    Validator singerValidator = ctx.getBean("singerValidator", Validator.class);
+    BeanPropertyBindingResult result = new BeanPropertyBindingResult(singer, "John");
 
-		ValidationUtils.invokeValidator(singerValidator, singer, result);
+    ValidationUtils.invokeValidator(singerValidator, singer, result);
 
-		List<ObjectError> errors = result.getAllErrors();
-		logger.info("No of validation errors: " + errors.size());
-		errors.forEach(e -> logger.info(e.getCode()));
+    List<ObjectError> errors = result.getAllErrors();
+    logger.info("No of validation errors: {}", errors.size());
+    errors.forEach(e -> logger.info(e.getCode()));
 
-		ctx.close();
-	}
+    ctx.close();
+  }
 }
