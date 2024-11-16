5f21460139070ae66ec552ff989c2b9f1baf3660
胡睿毅 <807776962@qq.com>
2024-11-16 11:38:40 +0800

    backup

---
+++ 1.8-log4j-slf4j/slf4j-config-log4j-v2/src/main/java/vip/fairy/HelloLog4J.java
@@ -0,0 +1,17 @@
+package vip.fairy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloLog4J {
+
+  public static void main(String[] args) {
+    Logger logger = LoggerFactory.getLogger(HelloLog4J.class);
+    logger.trace("TRACE..");
+    logger.debug("DEBUG..");
+    logger.info("INFO..");
+    logger.warn("WARN..");
+    logger.error("ERROR..");
+  }
+
+}
