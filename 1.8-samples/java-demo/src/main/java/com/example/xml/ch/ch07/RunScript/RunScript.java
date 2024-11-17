faa9b65bf1d51af2466c511e342ca755be1e1d45
胡睿毅 <807776962@qq.com>
2024-11-17 08:27:54 +0800

    ScriptEngineManager

--- 1.8-samples/java-demo/src/main/java/com/example/xml/ch/ch07/RunScript/RunScript.java
+++ 1.8-samples/java-demo/src/main/java/com/example/xml/ch/ch07/RunScript/RunScript.java
@@ -5,8 +5,10 @@ import static java.lang.System.err;
 import java.io.FileReader;
 import java.io.IOException;
 import javax.script.ScriptEngine;
+import javax.script.ScriptEngineFactory;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import org.junit.jupiter.api.Test;
 
 public class RunScript {
 
@@ -15,10 +17,8 @@ public class RunScript {
       err.println("usage: java RunScript script");
       return;
     }
-    ScriptEngineManager manager =
-        new ScriptEngineManager();
-    ScriptEngine engine =
-        manager.getEngineByName("nashorn");
+    ScriptEngineManager manager = new ScriptEngineManager();
+    ScriptEngine engine = manager.getEngineByName("nashorn");
     try {
       engine.eval(new FileReader(args[0]));
     } catch (ScriptException se) {
@@ -27,4 +27,29 @@ public class RunScript {
       err.println(ioe.getMessage());
     }
   }
+
+  @Test
+  void test1() {
+    ScriptEngineManager mgr = new ScriptEngineManager();
+
+    for (ScriptEngineFactory factory : mgr.getEngineFactories()) {
+      String engineName = factory.getEngineName();
+      String languageName = factory.getLanguageName();
+      String version = factory.getLanguageVersion();
+      System.out.println("Engine name: " + engineName + " Language: " + languageName + " Version: " + version);
+    }
+  }
+
+  @Test
+  void test2() {
+    ScriptEngineManager mgr = new ScriptEngineManager();
+    ScriptEngine jsEngine = mgr.getEngineByName("JavaScript");
+
+    try {
+      jsEngine.eval("print('Hello JavaScript in Java')");
+    } catch (ScriptException ex) {
+      ex.printStackTrace();
+    }
+  }
+
 }
