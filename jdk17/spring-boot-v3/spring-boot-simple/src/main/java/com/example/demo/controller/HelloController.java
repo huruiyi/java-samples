99d75c0993ebccbf6ade946f0b814a0eac654c20
胡睿毅 <807776962@qq.com>
2025-06-11 13:46:28 +0800

    backup

--- jdk17/spring-boot-v3/spring-boot-simple/src/main/java/com/example/demo/controller/HelloController.java
+++ jdk17/spring-boot-v3/spring-boot-simple/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,23 +1,19 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Greeting;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-//    @GetMapping(value = {"/", "/hello"}, produces = "text/json;charset=utf-8")
-//    public String hello() {
-//        return "你好，hello ";
-//    }
-
-  @GetMapping(value = {"/", "/hello"})
+  @GetMapping(value = {"/", "/hello"}, produces = "text/json;charset=utf-8")
   public String hello() {
     return "你好，hello ";
   }
-
 
   @GetMapping("/greetings/{name}")
   public ResponseEntity<Greeting> greetings(@PathVariable("name") String name) throws IllegalAccessException {
@@ -26,6 +22,5 @@ public class HelloController {
     }
     return ResponseEntity.ok(new Greeting("Hello " + name + "!"));
   }
-
 
 }
