c3105564d47e148a1fe9fa7abbec47ccf462f994
胡睿毅 <807776962@qq.com>
2025-12-05 09:42:58 +0800

    bak

--- jdk8/log-samples/log4j2-demo/src/main/java/vip/fairy/App.java
+++ jdk8/log-samples/log4j2-demo/src/main/java/vip/fairy/App.java
@@ -2,14 +2,13 @@ package vip.fairy;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.config.Configurator;
 
 public class App {
 
   private static final Logger logger = LogManager.getLogger();
 
   public static void main(String[] args) {
-    Configurator.initialize("TestClass", "log4j2-file-day.xml");
+    //Configurator.initialize("TestClass", "log4j2.properties");
 
     // 记录debug级别的信息
     logger.debug("This is debug message.");
@@ -20,6 +19,7 @@ public class App {
 
     for (int i = 0; i < 100000; i++) {
       logger.info("This is log message {}", i);
+      logger.error("This is error message.");
     }
     System.out.println("Hello World!");
   }
